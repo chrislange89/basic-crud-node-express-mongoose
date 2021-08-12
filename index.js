4c1fd19eaf9fa530baea05b2b73a210addef1311
Chris Lange <43015573+chrislange89@users.noreply.github.com>
2021-08-12 22:51:39 +0000

    Moved route handling into the mongoClient connection string

--- index.js
+++ index.js
@@ -6,18 +6,22 @@ const mongoURL = process.env['mongoConnectionString'];
 const app = express();
 
 mongoClient.connect(mongoURL, { useUnifiedTopology: true })
-          .then(client => {console.log('Connected to Database')})
-          .catch(error => console.error(error));
+  .then(client => {
+    console.log('Connected to Database')
+    const db = client.db('quotes');
 
-app.use(bodyParser.urlencoded({ extended: true}));
+    app.use(bodyParser.urlencoded({ extended: true}));
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
+    app.get('/', (req, res) => {
+      res.sendFile(__dirname + "/index.html");
+    });
 
-app.post('/quotes', (req, res) => {
-  console.log(req.body);
-})
+    app.post('/quotes', (req, res) => {
+      console.log(req.body);
+    });
+
+  })
+  .catch(error => console.error(error));
 
 const port = 3000 || process.env.port;
 
