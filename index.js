67b67f054066be84274acf845b5cfdab75565084
Chris Lange <43015573+chrislange89@users.noreply.github.com>
2021-08-12 22:47:47 +0000

    Basic connection string

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoClient = require('mongodb').MongoClient;
+const mongoURL = process.env['mongoConnectionString'];
 
 const app = express();
 
+mongoClient.connect(mongoURL, { useUnifiedTopology: true })
+          .then(client => {console.log('Connected to Database')})
+          .catch(error => console.error(error));
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
 app.get('/', (req, res) => {
-  // res.send('Hello Express app!')
-  console.log(__dirname);
   res.sendFile(__dirname + "/index.html");
 });
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body);
+})
 
 const port = 3000 || process.env.port;
 
