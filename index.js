71da58e824563cf7d2268c94f378fb4d76da09d9
Chris Lange <43015573+chrislange89@users.noreply.github.com>
2021-08-12 23:23:36 +0000

    Load quotes collection, basic css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoClient = require('mongodb').MongoClient;
 const mongoURL = process.env['mongoConnectionString'];
+const port = process.env.port || 3000;
 
 const app = express();
 
@@ -9,21 +10,37 @@ mongoClient.connect(mongoURL, { useUnifiedTopology: true })
   .then(client => {
     console.log('Connected to Database')
     const db = client.db('quotes');
+    const quotesCollection = db.collection('quotes');
+
+    app.set('view engine', 'ejs');
 
     app.use(bodyParser.urlencoded({ extended: true}));
+    app.use(express.static('css'));
 
     app.get('/', (req, res) => {
-      res.sendFile(__dirname + "/index.html");
+      const cursor = db.collection('quotes').find().toArray()
+      .then(results => {
+        console.log(results);
+        res.render('index.ejs', {quotes: results});
+      })
+      .catch(error => console.error(error));
+      console.log(cursor);
     });
 
     app.post('/quotes', (req, res) => {
       console.log(req.body);
+      quotesCollection.insertOne(req.body)
+      .then(result => {
+        console.log(result);
+        res.redirect('/');
+      })
+      .catch(error => console.error(error));
     });
 
   })
   .catch(error => console.error(error));
 
-const port = 3000 || process.env.port;
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
